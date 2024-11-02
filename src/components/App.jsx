114cfec7bcea13e49b0ab9d3ab684f90ce029ded
svbiz4u <136245961+svbiz4u@users.noreply.github.com>
2024-11-02 16:36:59 +0100

    com-021124-1636

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,125 +1,53 @@
+const Header = () => {
+  return ( 
+<header>
+    <div> Logo </div>
+      <nav>
+        <ul> 
+        <li>Home</li>
+        <li>About</li>
+        <li>Profile</li>
+        </ul>
+      </nav>
+  </header>
+)
+}
 
-// src/App.jsx
-// import Product from './Product';
+const Section = () => {
+  return (
+    <section>
+    <h2>hello world</h2>
+    <button>Click</button>    
+    <button>Hello</button>
+    <button>Submit</button>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab tempore odio accusantium ad placeat laudantium quod. Dignissimos, incidunt fuga, harum rem vel possimus perferendis eligendi, nostrum odio itaque quasi quis.     
+    </p>
 
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut est aliquam doloribus aspernatur sapiente, iusto similique temporibus quos magni animi suscipit harum eum culpa obcaecati error accusamus ea. Dolorem, non?</p>
+  </section>
+  )
+}
 
-import { useEffect, useState } from "react";
+const Footer = () => {
+  return (<footer>
+    <span>&copy; All rights reserved</span>
+  </footer>)
+}
 
 const App = () => {
-  const [clicks, setClicks] = useState(() => {
-    const savedClicks = window.localStorage.getItem("saved-clicks");
-    if (savedClicks !== null) {
-      return Number(savedClicks);
-    }
-    return 0;
-  });
-
-  useEffect(() => {
-    window.localStorage.setItem("saved-clicks", clicks);
-  }, [clicks]);
-
   return (
-    <div>
-      <button onClick={() => setClicks(clicks + 1)}>
-        You clicked {clicks} times
-      </button>
-      <button onClick={() => setClicks(0)}>Reset</button>
-    </div>
-  );
-};
+<div>
+ <Header />
+ <Section />
+ <Footer />
 
+  
+</div>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  )
+}
 
 
 
 export default App
 
-
-// export default function App() {
-//   return (
-//   //   <div>
-//   //   <h1>Best selling</h1> 
-
-//   //   <Product
-//   //     name="Tacos With Lime"
-//   //     imgUrl="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
-//   //     price={10.99}
-//   //   />
-//   //   <Product
-//   //     name="Fries and Burger"
-//   //     imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?dpr=2&h=480&w=640"
-//   //     price={14.29}
-//   //   />
-
-    
-
-//   // </div>
-//   );
-// }
-
-
-
-
-  
-
-
-
-
-
-
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
-
-// function App() {
-//   const [count, setCount] = useState(0)
-
-//   return (
-//     <>
-//       <div>
-//         <a href="https://vitejs.dev" target="_blank">
-//           <img src={viteLogo} className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://react.dev" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <h1>Vite + React</h1>
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.jsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p>
-//     </>
-//   )
-// }
-
-// export default App
